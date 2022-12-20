39dcfccbd2fb14e15c956dbf85b4053cf32ef100
Franco Ortega <francopdx@gmail.com>
2022-12-20 14:11:53 -0800

    create Post component to display title and id

---
+++ src/components/posts/Post.js
@@ -0,0 +1,22 @@
+export default function Post({ title, id }) {
+  return (
+    <li style={styles.post}>
+      <div style={styles.title}>{title}</div>
+      <div style={styles.id}>{id}</div>
+    </li>
+  );
+}
+
+const styles = {
+  post: {
+    borderColor: 'gray',
+    borderRadius: 10,
+    listStyleType: 'none'
+  },
+  title: {
+    fontSize: '1.5rem'
+  },
+  id: {
+    fontSize: '1.25rem'
+  }
+};
